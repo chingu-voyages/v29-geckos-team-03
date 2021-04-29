0bc4df30ce9fa6a3c8c958ba297bd917d3b0c9aa
kapre <kapremom@gail.com>
2021-04-29 10:30:45 -0400

    updated folder

--- skywatcher-frontend/src/App.js
+++ skywatcher-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing';
-import  Navbar from './pages/Navbar';
+import  Navbar from './components/Navbar';
 
 function App() {
   return (
