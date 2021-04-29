c75e4873d2b2226a2e5688726b6f6b12c82bb7f0
Kalkidan Alemu <kali.taye@gmail.com>
2021-04-28 22:32:18 -0400

    feature/WeatherDayByDay

--- skywatcher-frontend/src/pages/DisplayCard.js
+++ skywatcher-frontend/src/pages/DisplayCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Jumbotron, Card} from 'react-bootstrap'
+import { Container, Jumbotron, Card, Row, Col} from 'react-bootstrap'
 import '../index.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
@@ -46,6 +46,47 @@ const DisplayCard = () => {
 
           </Card.Body>
         </Card>
+
+        
+        
+        <Card style={{ width: '25rem' }}>
+          <Card.Body>
+            <Row xs={2} md={4} lg={6}>
+              <Col>Day</Col>
+              <Col>Day</Col>
+              <Col>Day</Col>
+              <Col>Day</Col>
+              <Col>Day</Col>
+            </Row>
+
+            <Row xs={2} md={4} lg={6}>
+              <Col><i className="fas fa-sun" style={{fontSize:24}}></i></Col>
+              <Col><i className="fas fa-sun" style={{fontSize:24}}></i></Col>
+              <Col><i className="fas fa-sun" style={{fontSize:24}}></i></Col>
+              <Col><i className="fas fa-sun" style={{fontSize:24}}></i></Col>
+              <Col><i className="fas fa-sun" style={{fontSize:24}}></i></Col>
+            </Row>
+
+            <Row xs={2} md={4} lg={6}>
+              <Col>13</Col>
+              <Col>15</Col>
+              <Col>17</Col>
+              <Col>14</Col>
+              <Col>14</Col>
+            </Row>
+        
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Text className="school-closing">Other</Card.Text>
+
+          </Card.Body>
+        </Card>
+        
+        
+
     </Container>
     
   )
--- skywatcher-frontend/src/pages/Landing.js
+++ skywatcher-frontend/src/pages/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Jumbotron, Card} from 'react-bootstrap'
+import { Container} from 'react-bootstrap'
 import DisplayCard from './DisplayCard.js'
 import '../index.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
