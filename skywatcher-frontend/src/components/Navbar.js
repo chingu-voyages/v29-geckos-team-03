b7cbbd0aecdfd0e9a08296a6104a44348296d16c
kapre <kapremom@gail.com>
2021-05-05 14:51:06 -0400

    added links to navbar

--- skywatcher-frontend/src/components/Navbar.js
+++ skywatcher-frontend/src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import * as ReactBootStrap from "react-bootstrap";
 import "../App.js";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <ReactBootStrap.Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-            <ReactBootStrap.Navbar.Brand href="#home">Skywatcher</ReactBootStrap.Navbar.Brand>
+            <ReactBootStrap.Navbar.Brand as={Link} to='/'>Skywatcher</ReactBootStrap.Navbar.Brand>
             <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                 <ReactBootStrap.Nav className="mr-auto">
                         </ReactBootStrap.Nav>
                         <ReactBootStrap.Nav>
-                        <ReactBootStrap.Nav.Link href="#api">API Page</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link as={Link} to='/interactwithdata'>API Page</ReactBootStrap.Nav.Link>
                         <ReactBootStrap.Nav.Link eventKey={2} href="#about">
                             About
                         </ReactBootStrap.Nav.Link>
