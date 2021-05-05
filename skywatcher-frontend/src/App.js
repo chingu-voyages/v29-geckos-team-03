67cfb44b3fc513fe967ad3df78f7735ed364f9b3
Edwin Agbenyega <eddybanks03@gmail.com>
2021-05-05 12:52:43 -0600

    Add location component for illustration purposes

--- skywatcher-frontend/src/App.js
+++ skywatcher-frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing';
 import  Navbar from './components/Navbar';
 import InteractWithData from './pages/InteractWithData';
+import Locations from './pages/Locations';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/' component={Landing} />
+        <Route exact path='/locations' component={Locations} />
         <Route exact path='/interactwithdata' component={InteractWithData} />
       </Switch>
       </BrowserRouter>
