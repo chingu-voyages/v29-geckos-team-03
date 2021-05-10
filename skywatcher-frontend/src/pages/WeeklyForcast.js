dd7e81542a5b8f8d9a8d9db03fe0ea4b64e95c51
Kalkidan Alemu <kali.taye@gmail.com>
2021-05-10 15:13:34 -0400

    Feature: temperature

--- skywatcher-frontend/src/pages/WeeklyForcast.js
+++ skywatcher-frontend/src/pages/WeeklyForcast.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Jumbotron, Card, Row, Col} from 'react-bootstrap'
+import {Card, Row, Col} from 'react-bootstrap'
 import '../index.css';
 
 class WeeklyForcast extends React.Component {
@@ -11,19 +11,23 @@ class WeeklyForcast extends React.Component {
   }
 
   async componentDidMount(){
-    const api_url = `https://api.openweathermap.org/data/2.5/forecast?q=new%20york&cnt=6&units=metric&appid=0aeeb80f1822726609f7a5570cc0992c`
+    const api_url = `https://api.openweathermap.org/data/2.5/forecast?q=new%20york&cnt=40&units=metric&appid=0aeeb80f1822726609f7a5570cc0992c`
     const response = await fetch(api_url);
     const data = await response.json()
-    this.setState({ day: data.list[0], loading: false})
-    this.setState({ weatherIcon: data.list[0].weather[0].icon, loading: false})
-    // this.setState({ temperature: data.list[0], loading: false})
 
-    console.log(data.list[0].main.temp)
-    console.log(data.list[0].dt_txt)
+    for (var i = 1; i < 6; i++) {
+      
+        this.setState({ day: data.list[i], loading: false})
+        this.setState({ weatherIcon: data.list[i].weather[0].icon, loading: false})
+        this.setState({ temperature: data.list[i].main.temp, loading: false})
+    
 
-    console.log(data.list[0])
+    // console.log(data.list[i].main.temp)
+    // console.log(data.list[i].dt_txt)
+
+    console.log(data)
   }
-
+}
   render(){
 
     if(this.state.loading){
@@ -38,16 +42,11 @@ class WeeklyForcast extends React.Component {
                 <Card.Body>
                   <Row xs={2} md={4} lg={6}>
                     <Col>{this.state.day.dt_txt}</Col>
-                    <Col>Day</Col>
-                    <Col>Day</Col>
-                    <Col>Day</Col>
-                    <Col>Day</Col>
-                    <Col>Day</Col>
+                    
                   </Row>
 
                   <Row xs={2} md={4} lg={6}>
-                    <Col><img src={`http://openweathermap.org/img/w/${this.state.weatherIcon}.png`}/></Col>
-                    <Col></Col>
+                    <Col><img src={`http://openweathermap.org/img/w/${this.state.weatherIcon}.png` } alt =""/></Col>
                     <Col></Col>
                     <Col></Col>
                     <Col></Col>
@@ -55,12 +54,11 @@ class WeeklyForcast extends React.Component {
                   </Row>
 
                   <Row xs={2} md={4} lg={6}>
-                    {/* <Col>{this.state.temperature.main.temp}</Col> */}
+                    <Col>{this.state.temperature}</Col>
                     <Col>15</Col>
                     <Col>17</Col>
                     <Col>14</Col>
                     <Col>14</Col>
-                    <Col>16</Col>
                   </Row>
               
                 </Card.Body> 
