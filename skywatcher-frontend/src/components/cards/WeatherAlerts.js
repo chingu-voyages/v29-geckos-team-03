cddb33bed0df55f1da55b8a580060748b454c35e
kapre <kapremom@gail.com>
2021-05-14 13:35:38 -0400

    added weatheralerts info to card

--- skywatcher-frontend/src/components/cards/WeatherAlerts.js
+++ skywatcher-frontend/src/components/cards/WeatherAlerts.js
@@ -5,12 +5,26 @@ import { CardTitle, StyledCard } from "../CustomStyling";
 const WeatherAlerts = ({ lat, lon }) => {
   const { data, error, loading } = useFetchWeatherAlerts({ lat, lon });
 
+  const alerts = [
+    {
+      lat: "40.7143",
+      lon: "-74.006",
+      timezone: "America/New_York",
+      timezone_offset: "-14400",
+    },
+  ];
+
   if (loading) return <p>Still Loading!</p>;
   if (error) throw error;
   return (
     <StyledCard>
       <CardTitle>Weather Alerts</CardTitle>
-      <p>{JSON.stringify(data)}</p>
+      <>
+        <p>Timezone {alerts[0].timezone}</p>
+        <p>Latuide {alerts[0].lat}</p>
+        <p>Longitude {alerts[0].lon}</p>
+        <p>Timezone offset {alerts[0].timezone_offset}</p>
+      </>
     </StyledCard>
   );
 };
