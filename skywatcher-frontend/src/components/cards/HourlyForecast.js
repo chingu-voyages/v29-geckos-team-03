5d271232039f8061189b3c914b3c084fd0b5a8ff
Kalkidan Alemu <kali.taye@gmail.com>
2021-05-23 08:05:19 -0400

    Style: Hourly Forcast

--- skywatcher-frontend/src/components/cards/HourlyForecast.js
+++ skywatcher-frontend/src/components/cards/HourlyForecast.js
@@ -11,17 +11,20 @@ const HourlyForecast = ({ lat, lon, unit }) => {
   console.log(data);
 
   return (
-    <Card.Body>
+    <Card.Body id="hourly-forcast">
       {data.hourly.map((item, idx) => (
         <Row key={`hourly-${idx}`}>
           {ToDayShort(item.dt)}, {ToTime(item.dt)}
           <img
+            id="hourly-img"
             src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`}
             alt=""
           />
           <hr />
-          <p>Temp: {Number(item.temp).toFixed(0)}&deg;C </p>
-          <p>Feels Like:{Number(item.feels_like).toFixed(0)}&deg;C</p>
+          <p id="hourly-temp">{Number(item.temp).toFixed(0)}&deg;C </p>
+          <p id="hourly-feelsLike">
+            Feels Like: {Number(item.feels_like).toFixed(0)}&deg;C
+          </p>
         </Row>
       ))}
     </Card.Body>
